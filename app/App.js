0806c80f325c5f7868329304d92f47ed22b98084
Gregory Monroe II <gregthemusician@gmail.com>
2021-02-18 16:18:47 -0500

    added boilerplate to App.js

---
+++ app/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'normalize.css'
+import './App.scss'
+
+const App = () => {
+    <div>
+        <h1>Hello React</h1>
+        <br/>
+        <h2>insert image here</h2>
+    </div>
+}
+
+export default App
